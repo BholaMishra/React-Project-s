3301742f8c37ab251c7cdf314912f7b1240143c2
Snehjaiswal <snehj121@gmail.com>
2022-07-07 21:12:26 +0530

    normal practice

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,10 +1,14 @@
+import Navbar from "./Components/Navbar";
 import Home from "./Components/home"
+import NewComponent from "./Components/Card";
+
 
 function App() {
   return (
     <>
+     <Navbar />
     <Home />
-   
+  
     </>
   );
 }
