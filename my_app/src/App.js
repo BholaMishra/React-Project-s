6e9a115925c92ca068a1e491da1dd20a85a04325
Snehjaiswal <snehj121@gmail.com>
2022-07-07 20:28:05 +0530

    push

---
+++ my_app/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./Components/home"
+
+function App() {
+  return (
+    <>
+    <Home />
+   
+    </>
+  );
+}
+
+export default App;
---
+++ my_app/src/Components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <h1>Welcome Home page...🤞</h1>
+    </div>
+  )
+}
+
+export default home
